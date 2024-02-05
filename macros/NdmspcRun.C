5484a9815d58a74eb86a34ec1ddcfff88a24bd23
Martin Vala <vala.martin@gmail.com>
2024-02-05 13:15:47 +0000

    Resolve "Support + in objects"

--- macros/NdmspcRun.C
+++ macros/NdmspcRun.C
@@ -13,9 +13,10 @@
 
 using json = nlohmann::json;
 
-json cfg;
-void NdmspcDefaultConfig(json& cfg);
-TList* NdmspcProcess(TList* inputList = nullptr, json cfg = R"()"_json, THnSparse* finalResults = nullptr, Int_t* point = nullptr);
+json    cfg;
+void    NdmspcDefaultConfig(json & cfg);
+TList * NdmspcProcess(TList * inputList = nullptr, json cfg = R"()"_json, THnSparse * finalResults = nullptr,
+                      Int_t * point = nullptr);
 
 json ndmspcBase = R"({
   "output": {
@@ -31,20 +32,20 @@ json ndmspcBase = R"({
 
 })"_json;
 
-int _ndmspcVerbose = 0;
-TFile* _currentInputFile = nullptr;
-TList* _currentInputObjects = nullptr;
-THnSparse* _finalResults = nullptr;
-Int_t _currentPoint[10];
+int         _ndmspcVerbose       = 0;
+TFile *     _currentInputFile    = nullptr;
+TList *     _currentInputObjects = nullptr;
+THnSparse * _finalResults        = nullptr;
+Int_t       _currentPoint[10];
 
-void NdmspcLoadConfig(TString configFile, json& cfg, bool show = true)
+void NdmspcLoadConfig(TString configFile, json & cfg, bool show = true)
 {
   NdmspcDefaultConfig(cfg);
   cfg.merge_patch(ndmspcBase);
 
   if (!configFile.IsNull() && !gSystem->AccessPathName(gSystem->ExpandPathName(configFile.Data()))) {
     std::ifstream f(configFile);
-    json cfgJson = json::parse(f);
+    json          cfgJson = json::parse(f);
     cfg.merge_patch(cfgJson);
   }
 
@@ -56,8 +57,7 @@ void NdmspcLoadConfig(TString configFile, json& cfg, bool show = true)
     verbose = cfg["verbose"].get<int>();
   }
 
-  if (verbose >= 1)
-    Printf("%s", cfg.dump(2).c_str());
+  if (verbose >= 1) Printf("%s", cfg.dump(2).c_str());
 
   // handle specific options
   if (cfg["file"]["cache"].is_string() && !cfg["file"]["cache"].get<std::string>().empty())
@@ -66,39 +66,39 @@ void NdmspcLoadConfig(TString configFile, json& cfg, bool show = true)
   TH1::AddDirectory(kFALSE);
 }
 
-TList* NdmspcInit(json cfg)
+TList * NdmspcInit(json cfg)
 {
 
-  if (_currentInputFile && _currentInputObjects)
-    return _currentInputObjects;
+  if (_currentInputFile && _currentInputObjects) return _currentInputObjects;
 
   if (cfg["data"]["file"].get<std::string>().empty()) {
     Printf("Error: Input file is empty !!! Aborting ...");
     return nullptr;
   }
-  if (_ndmspcVerbose >= 2)
-    Printf("Opening file '%s' ...", cfg["data"]["file"].get<std::string>().c_str());
+  if (_ndmspcVerbose >= 2) Printf("Opening file '%s' ...", cfg["data"]["file"].get<std::string>().c_str());
   _currentInputFile = TFile::Open(cfg["data"]["file"].get<std::string>().c_str());
   if (!_currentInputFile) {
     Printf("Error: Cannot open file '%s' !", cfg["data"]["file"].get<std::string>().c_str());
     return nullptr;
   }
 
-  if (!_currentInputObjects)
-    _currentInputObjects = new TList();
+  if (!_currentInputObjects) _currentInputObjects = new TList();
 
-  TObject* o;
-  for (auto& obj : cfg["data"]["objects"]) {
-    if (_ndmspcVerbose >= 3)
-      Printf("obj=%s", obj.get<std::string>().c_str());
-    if (obj.get<std::string>().empty())
-      continue;
-    o = _currentInputFile->Get(obj.get<std::string>().c_str());
-    if (!o) {
-      Printf("Error : Could not open '%s' from file '%s' !!! Skipping ...", obj.get<std::string>().c_str(), cfg["data"]["file"].get<std::string>().c_str());
-      return nullptr;
+  TObject * o;
+  for (auto & obj : cfg["data"]["objects"]) {
+    if (_ndmspcVerbose >= 3) Printf("obj=%s", obj.get<std::string>().c_str());
+    if (!obj.get<std::string>().empty()) {
+      o = _currentInputFile->Get(obj.get<std::string>().c_str());
+      if (!o) {
+        Printf("Error : Could not open '%s' from file '%s' !!! Skipping ...", obj.get<std::string>().c_str(),
+               cfg["data"]["file"].get<std::string>().c_str());
+        return nullptr;
+      }
+      _currentInputObjects->Add(o);
     }
-    _currentInputObjects->Add(o);
+    else {
+      _currentInputObjects->Add(new TNamed("empty", "Empty object"));
+    }
   }
 
   if (_currentInputObjects->GetEntries() != cfg["data"]["objects"].size()) {
@@ -108,23 +108,21 @@ TList* NdmspcInit(json cfg)
   return _currentInputObjects;
 }
 
-bool NdmspcApplyCuts(json& cfg)
+bool NdmspcApplyCuts(json & cfg)
 {
-  TString titlePostfix = "";
-  THnSparse* s;
+  TString     titlePostfix = "";
+  THnSparse * s;
 
-  int iCut = 0;
+  int iCut            = 0;
   _currentPoint[iCut] = 0;
   for (size_t i = 0; i < _currentInputObjects->GetEntries(); i++) {
-    s = (THnSparse*)_currentInputObjects->At(i);
+    s    = (THnSparse *)_currentInputObjects->At(i);
     iCut = 1;
-    for (auto& cut : cfg["cuts"]) {
+    for (auto & cut : cfg["cuts"]) {
 
-      if (cut.is_null())
-        continue;
+      if (cut.is_null()) continue;
 
-      if (cut["enabled"].is_boolean() && cut["enabled"].get<bool>() == false)
-        continue;
+      if (cut["enabled"].is_boolean() && cut["enabled"].get<bool>() == false) continue;
 
       if (cut["axis"].is_string() && cut["axis"].get<std::string>().empty()) {
         std::cerr << "Error: Axis name is empty ('" << cut << "') !!! Exiting ..." << std::endl;
@@ -144,7 +142,9 @@ bool NdmspcApplyCuts(json& cfg)
       _currentPoint[iCut] = cut["bin"]["min"].get<int>();
 
       if (i == 0)
-        titlePostfix += TString::Format("%s[%.2f,%.2f] ", s->GetAxis(id)->GetName(), s->GetAxis(id)->GetBinLowEdge(cut["bin"]["min"].get<int>()), s->GetAxis(id)->GetBinUpEdge(cut["bin"]["max"].get<int>()));
+        titlePostfix += TString::Format("%s[%.2f,%.2f] ", s->GetAxis(id)->GetName(),
+                                        s->GetAxis(id)->GetBinLowEdge(cut["bin"]["min"].get<int>()),
+                                        s->GetAxis(id)->GetBinUpEdge(cut["bin"]["max"].get<int>()));
 
       iCut++;
     }
@@ -158,7 +158,7 @@ bool NdmspcApplyCuts(json& cfg)
   return true;
 }
 
-void NdmspcSaveFile(TList* outputList, json cfg)
+void NdmspcSaveFile(TList * outputList, json cfg)
 {
 
   TString outputFileName;
@@ -167,20 +167,20 @@ void NdmspcSaveFile(TList* outputList, json cfg)
   if (!cfg["output"]["dir"].get<std::string>().empty())
     outputFileName += cfg["output"]["dir"].get<std::string>().c_str();
   if (cfg["cuts"].is_array() && !outputFileName.IsNull())
-    for (auto& cut : cfg["cuts"]) {
+    for (auto & cut : cfg["cuts"]) {
       outputFileName += TString::Format("/%d", cut["bin"]["min"].get<int>());
     }
-  if (!outputFileName.IsNull())
-    outputFileName += "/";
+  if (!outputFileName.IsNull()) outputFileName += "/";
   if (!cfg["output"]["file"].get<std::string>().empty())
     outputFileName += cfg["output"]["file"].get<std::string>().c_str();
 
-  TFile* fOut = TFile::Open(TString::Format("%s%s", outputFileName.Data(), cfg["output"]["opt"].get<std::string>().c_str()).Data(), "RECREATE");
+  TFile * fOut = TFile::Open(
+      TString::Format("%s%s", outputFileName.Data(), cfg["output"]["opt"].get<std::string>().c_str()).Data(),
+      "RECREATE");
   outputList->Write();
   fOut->Close();
 
-  if (_ndmspcVerbose >= 0)
-    Printf("Objects stored in '%s'", outputFileName.Data());
+  if (_ndmspcVerbose >= 0) Printf("Objects stored in '%s'", outputFileName.Data());
 }
 void NdmspcDestroy()
 {
@@ -198,14 +198,14 @@ void NdmspcDestroy()
   }
 }
 
-THnSparse* CreateResult(json& cfg)
+THnSparse * CreateResult(json & cfg)
 {
 
-  THnSparse* s = (THnSparse*)_currentInputObjects->At(0);
-  const int ndims = cfg["cuts"].size() + 1;
-  Int_t bins[ndims];
-  Double_t xmin[ndims];
-  Double_t xmax[ndims];
+  THnSparse *              s     = (THnSparse *)_currentInputObjects->At(0);
+  const int                ndims = cfg["cuts"].size() + 1;
+  Int_t                    bins[ndims];
+  Double_t                 xmin[ndims];
+  Double_t                 xmax[ndims];
   std::vector<std::string> names;
   std::vector<std::string> titles;
 
@@ -216,12 +216,10 @@ THnSparse* CreateResult(json& cfg)
   titles.push_back("Parameters");
 
   int i = 1;
-  for (auto& cut : cfg["cuts"]) {
-    if (_ndmspcVerbose >= 3)
-      std::cout << "CreateResult() : " << cut.dump() << std::endl;
-    TAxis* a = (TAxis*)s->GetListOfAxes()->FindObject(cut["axis"].get<std::string>().c_str());
-    if (a == nullptr)
-      return nullptr;
+  for (auto & cut : cfg["cuts"]) {
+    if (_ndmspcVerbose >= 3) std::cout << "CreateResult() : " << cut.dump() << std::endl;
+    TAxis * a = (TAxis *)s->GetListOfAxes()->FindObject(cut["axis"].get<std::string>().c_str());
+    if (a == nullptr) return nullptr;
     bins[i] = a->GetNbins();
     xmin[i] = a->GetXmin();
     xmax[i] = a->GetXmax();
@@ -230,60 +228,55 @@ THnSparse* CreateResult(json& cfg)
     i++;
   }
 
-  THnSparse* fres = new THnSparseD("results", "Final results", ndims, bins, xmin, xmax);
-  i = 0;
-  for (auto& n : names) {
+  THnSparse * fres = new THnSparseD("results", "Final results", ndims, bins, xmin, xmax);
+  i                = 0;
+  for (auto & n : names) {
     fres->GetAxis(i)->SetNameTitle(names.at(i).c_str(), titles.at(i).c_str());
     i++;
   }
 
   if (!cfg["result"]["names"].is_null() && cfg["result"]["names"].is_array()) {
     i = 1;
-    for (auto& n : cfg["result"]["names"]) {
-      TAxis* a = fres->GetAxis(0);
+    for (auto & n : cfg["result"]["names"]) {
+      TAxis * a = fres->GetAxis(0);
       a->SetBinLabel(i++, n.get<std::string>().c_str());
     }
   }
 
-  if (_ndmspcVerbose >= 3)
-    fres->Print("all");
+  if (_ndmspcVerbose >= 3) fres->Print("all");
 
   return fres;
 }
 
-bool NdmspcProcessSinglePoint(json& cfg)
+bool NdmspcProcessSinglePoint(json & cfg)
 {
-  if (!NdmspcApplyCuts(cfg))
-    return false;
+  if (!NdmspcApplyCuts(cfg)) return false;
 
-  if (_finalResults == nullptr)
-    delete _finalResults;
+  if (_finalResults == nullptr) delete _finalResults;
   _finalResults = CreateResult(cfg);
 
-  TList* outputList = NdmspcProcess(_currentInputObjects, cfg, _finalResults, _currentPoint);
+  TList * outputList = NdmspcProcess(_currentInputObjects, cfg, _finalResults, _currentPoint);
 
-  if (outputList)
-    NdmspcSaveFile(outputList, cfg);
+  if (outputList) NdmspcSaveFile(outputList, cfg);
 
   return true;
 }
 
-bool NdmspcProcessRecursive(int i, json& cfg)
+bool NdmspcProcessRecursive(int i, json & cfg)
 {
   if (i < 0) {
     return NdmspcProcessSinglePoint(cfg);
   }
 
-  THnSparse* s = (THnSparse*)_currentInputObjects->At(0);
-  TAxis* a = (TAxis*)s->GetListOfAxes()->FindObject(cfg["cuts"][i]["axis"].get<std::string>().c_str());
-  if (a == nullptr)
-    return false;
+  THnSparse * s = (THnSparse *)_currentInputObjects->At(0);
+  TAxis *     a = (TAxis *)s->GetListOfAxes()->FindObject(cfg["cuts"][i]["axis"].get<std::string>().c_str());
+  if (a == nullptr) return false;
   Int_t start = 1;
-  Int_t end = a->GetNbins();
+  Int_t end   = a->GetNbins();
 
   if (cfg["process"]["ranges"].is_array()) {
     start = cfg["process"]["ranges"][i][0];
-    end = cfg["process"]["ranges"][i][1];
+    end   = cfg["process"]["ranges"][i][1];
   }
 
   for (Int_t iBin = start; iBin <= end; iBin++) {
@@ -300,25 +293,24 @@ int NdmspcRun(TString cfgFile = "", bool showConfig = true)
   NdmspcLoadConfig(cfgFile.Data(), cfg, showConfig);
 
   std::string type;
-  if (cfg["process"]["type"].is_string())
-    type = cfg["process"]["type"].get<std::string>();
+  if (cfg["process"]["type"].is_string()) type = cfg["process"]["type"].get<std::string>();
 
   if (type.empty()) {
     Printf("Warning: [process][type] is missing or is empty in configuration !!! Setting it ot 'single' ...");
-    type = "single";
+    type                   = "single";
     cfg["process"]["type"] = type;
   }
 
-  TList* inputList = NdmspcInit(cfg);
-  if (inputList == nullptr)
-    return 1;
+  TList * inputList = NdmspcInit(cfg);
+  if (inputList == nullptr) return 1;
 
   if (!type.compare("single")) {
-    if (!NdmspcProcessSinglePoint(cfg))
-      return 3;
-  } else if (!type.compare("all")) {
+    if (!NdmspcProcessSinglePoint(cfg)) return 3;
+  }
+  else if (!type.compare("all")) {
     NdmspcProcessRecursive(cfg["cuts"].size() - 1, cfg);
-  } else {
+  }
+  else {
     Printf("Error: Value [process][type]='%s' is not supported !!! Exiting ...", type.c_str());
     return 4;
   }
