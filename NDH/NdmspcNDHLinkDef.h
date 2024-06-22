5780264b896659a59f91da98cbaf5dbf46458951
Martin Vala <vala.martin@gmail.com>
2024-06-22 05:46:10 +0000

    Resolve "TArrayL.h include missing"

--- NDH/NdmspcNDHLinkDef.h
+++ NDH/NdmspcNDHLinkDef.h
@@ -7,7 +7,7 @@
 #pragma link C++ class NDH::HnSparse + ;
 #pragma link C++ class NDH::HnSparseT<TArrayD> + ;
 #pragma link C++ class NDH::HnSparseT<TArrayF> + ;
-#pragma link C++ class NDH::HnSparseT<TArrayL> + ;
+#pragma link C++ class NDH::HnSparseT<TArrayL64> + ;
 #pragma link C++ class NDH::HnSparseT<TArrayI> + ;
 #pragma link C++ class NDH::HnSparseT<TArrayS> + ;
 #pragma link C++ class NDH::HnSparseT<TArrayC> + ;
