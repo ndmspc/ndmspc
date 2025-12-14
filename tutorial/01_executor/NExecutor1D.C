bf8f804e31ad956eb9086749d61ce92e47e705a9
Hajnalka Haneszova <haneszova.h@gmail.com>
2025-12-14 20:49:01 +0000

    Resolve "Executor1D clean up"

--- tutorial/01_executor/NExecutor1D.C
+++ tutorial/01_executor/NExecutor1D.C
@@ -16,26 +16,24 @@ void NExecutor1D(std::string outFile = "NExecutor1D.root")
   // add axis to the list of axes
   axes->Add(a1);
 
-  // Create an NGbnTree from the list of axes
+  // Create an NGnTree from the list of axes
   Ndmspc::NGnTree * ngnt = new Ndmspc::NGnTree(axes, outFile);
 
   // Define the binning for the axes
   std::map<std::string, std::vector<std::vector<int>>> b;
+
   // Set binning for axis1 (rebin to 1 bin)
   b["axis1"] = {{1}};
+
   // Create the binning definition with name "default" in the NGnTree
   ngnt->GetBinning()->AddBinningDefinition("default", b);
 
   // Define the processing function
   Ndmspc::NHnSparseProcessFuncPtr processFunc = [](Ndmspc::NBinningPoint * point, TList * output, TList * outputPoint,
                                                    int threadId) {
-    // print the binning point information
-    // point->Print();
-
-    // print the title of the binning point
-    NLogInfo("title : %s", point->GetString().c_str());
-    NLogInfo("  min=%.2f max=%.2f label=%s", point->GetBinMin("axis1"), point->GetBinMax("axis1"),
-             point->GetBinLabel("axis1").c_str());
+    // print info about the binning point
+    NLogInfo("title: %s", point->GetString().c_str());
+    NLogInfo("  min=%.2f max=%.2f", point->GetBinMin("axis1"), point->GetBinMax("axis1"));
   };
 
   // execute the processing function
